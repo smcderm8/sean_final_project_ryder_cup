38724137e54f7ccd2597eb185ef619c60ba56d04
Raghu Betina <raghu@firstdraft.com>
2017-08-07 21:05:24 +0000

    Whitelist columns in TeeTime for ActiveAdmin

--- app/admin/tee_time.rb
+++ app/admin/tee_time.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register TeeTime do
 
+ permit_params :time, :player_id, :match_id, :team_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
