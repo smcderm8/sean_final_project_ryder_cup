bfecd1778f481f59158c6ddf30f395af59ac751c
Raghu Betina <raghu@firstdraft.com>
2017-08-07 21:03:53 +0000

    Generate teams resource with starter:resource

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,81 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+
+    render("teams/index.html.erb")
+  end
+
+  def show
+    @team = Team.find(params[:id])
+
+    render("teams/show.html.erb")
+  end
+
+  def new
+    @team = Team.new
+
+    render("teams/new.html.erb")
+  end
+
+  def create
+    @team = Team.new
+
+    @team.name = params[:name]
+    @team.photo_url = params[:photo_url]
+
+    save_status = @team.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/teams/new", "/create_team"
+        redirect_to("/teams")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Team created successfully.")
+      end
+    else
+      render("teams/new.html.erb")
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+
+    render("teams/edit.html.erb")
+  end
+
+  def update
+    @team = Team.find(params[:id])
+
+    @team.name = params[:name]
+    @team.photo_url = params[:photo_url]
+
+    save_status = @team.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/teams/#{@team.id}/edit", "/update_team"
+        redirect_to("/teams/#{@team.id}", :notice => "Team updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Team updated successfully.")
+      end
+    else
+      render("teams/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+
+    @team.destroy
+
+    if URI(request.referer).path == "/teams/#{@team.id}"
+      redirect_to("/", :notice => "Team deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Team deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Team resource:
+  # CREATE
+  get "/teams/new", :controller => "teams", :action => "new"
+  post "/create_team", :controller => "teams", :action => "create"
+
+  # READ
+  get "/teams", :controller => "teams", :action => "index"
+  get "/teams/:id", :controller => "teams", :action => "show"
+
+  # UPDATE
+  get "/teams/:id/edit", :controller => "teams", :action => "edit"
+  post "/update_team/:id", :controller => "teams", :action => "update"
+
+  # DELETE
+  get "/delete_team/:id", :controller => "teams", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
