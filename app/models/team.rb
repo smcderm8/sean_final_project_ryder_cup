5083244fe7a534c8d2cf7c6a81c9ae0e625eb90a
Raghu Betina <raghu@firstdraft.com>
2017-08-07 21:04:37 +0000

    Direct association between teams and tee_times

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
   # Direct associations
 
+  has_many   :tee_times
+
   has_many   :players
 
   # Indirect associations
--- app/models/tee_time.rb
+++ app/models/tee_time.rb
@@ -1,6 +1,9 @@
 class TeeTime < ApplicationRecord
   # Direct associations
 
+  belongs_to :team,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
