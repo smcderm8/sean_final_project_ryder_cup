073e485d17813e5387cba06ac231c83d0e7bdd99
Raghu Betina <raghu@firstdraft.com>
2017-08-07 21:05:16 +0000

    Whitelist columns in Course for ActiveAdmin

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Course do
 
+ permit_params :image_url, :name, :address, :par, :rating, :slope
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
